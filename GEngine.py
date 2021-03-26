183445ecdd38cb022c1d290527e4a05e96d14670
AbhishekBelgaonkar <80008858+AbhishekBelgaonkar@users.noreply.github.com>
2021-03-26 12:58:28 +0400

    engine and init

---
+++ GEngine.py
@@ -0,0 +1,15 @@
+# determines the valid moves
+class game():
+	def __init__(self): #constructor
+		self.board = [ #first char of eleent represents color "--" is empty space
+			["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+			["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+			["--", "--", "--", "--", "--", "--", "--", "--"],
+			["--", "--", "--", "--", "--", "--", "--", "--"],
+			["--", "--", "--", "--", "--", "--", "--", "--"],
+			["--", "--", "--", "--", "--", "--", "--", "--"],
+			["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+			["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+		]
+		WhiteToMove = True
+		self.movelog = []
